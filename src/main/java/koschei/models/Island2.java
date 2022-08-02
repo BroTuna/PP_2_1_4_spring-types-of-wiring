7e9501b56f16c23d5bb205a39a5250fc5c3a02d3
BroTuna <shine191289@gmail.com>
2022-08-02 18:17:12 +0300

    рип

--- src/main/java/koschei/models/Island2.java
+++ src/main/java/koschei/models/Island2.java
@@ -1,10 +1,12 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+
 public class Island2 {
 
     private Wood3 wood;
 
-    public Island2(Wood3 wood) {
+    public Island2( Wood3 wood) {
         this.wood = wood;
     }
 
